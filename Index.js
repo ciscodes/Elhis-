b066fd057151da5bd3f51da598ae3ae48af287bc
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2021-10-06 13:39:13 +0200

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,1 +1,16 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const message =
+  "Thank you for submitting your request. We will respond shortly.";
+
+document.getElementById("ticketForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+  });
+
 
